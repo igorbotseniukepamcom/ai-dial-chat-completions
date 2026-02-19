675fd39d3e56b307bdfe76ca58021ea1b42541c1
igor-botseniuk-danone <141125367+igor-botseniuk-danone@users.noreply.github.com>
2026-02-19 10:02:12 +0200

    Task 1. Add client and app code.

---
+++ task/check.py
@@ -0,0 +1,8 @@
+import os
+
+DEFAULT_SYSTEM_PROMPT = "You are an assistant who answers concisely and informatively."
+DIAL_ENDPOINT = "https://ai-proxy.lab.epam.com"
+API_KEY = os.getenv('DIAL_API_KEY', '')
+
+print('Check')
+print(API_KEY)
--- task/clients/custom_client.py
+++ task/clients/custom_client.py
@@ -28,7 +28,28 @@ class DialClient:
         #   - json: request_data from step 2
         # 4. Get content from response, print it and return message with assistant role and content
         # 5. If status code != 200 then raise Exception with format: f"HTTP {response.status_code}: {response.text}"
-        raise NotImplementedError
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
+
+        request_data = {
+            "messages": [msg.to_dict() for msg in messages]
+        }
+
+        response = requests.post(url=self._endpoint, headers=headers, json=request_data)
+
+
+        if response.status_code == 200:
+            data = response.json()
+            choices = data.get("choices", [])
+            if choices:
+                content = choices[0].get("message", {}).get("content")
+                print(content)
+                return Message(Role.AI, content)
+            raise ValueError("No Choice has been present in the response")
+        else:
+            raise Exception(f"HTTP {response.status_code}: {response.text}")
 
     async def stream_completion(self, messages: list[Message]) -> Message:
         #TODO:
@@ -46,5 +67,41 @@ class DialClient:
         #    - Use 'async with' context manager for response
         # 6. Get content from chunks (don't forget that chunk start with `data: `, final chunk is `data: [DONE]`), print
         #    chunks, collect them and return as assistant message
-        raise NotImplementedError
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
+        request_data = {
+            "stream": True,
+            "messages": [msg.to_dict() for msg in messages]
+        }
+        contents = []
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=self._endpoint, headers=headers, json=request_data) as response:
+                if response.status == 200:
+                    async for line in response.content:
+                        line_str = line.decode('utf-8').strip()
+                        if line_str.startswith("data: "):
+                            data = line_str[6:].strip()
+                            if data != "[DONE]":
+                                content_snippet = self._get_content_snippet(data)
+                                print(content_snippet, end='')
+                                contents.append(content_snippet)
+                            else:
+                                print()
+                else:
+                    error_text = await response.text()
+                    print(f"{response.status} {error_text}")
+                return Message(role=Role.AI, content=''.join(contents))
+
+    def _get_content_snippet(self, data: str) -> str:
+        """
+        Extract content from streaming data chunk.
+        """
+        data = json.loads(data)
+        if choices := data.get("choices"):
+            delta = choices[0].get("delta", {})
+            return delta.get("content", '')
+        return ''
 
